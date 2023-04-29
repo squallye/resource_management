74e2a4cdc305b6c795addd5df41eb63b8d717b4f
squallye <netcock@gmail.com>
2023-04-29 19:15:24 -0400

    Add files via upload

--- operation.py
+++ operation.py
@@ -5,10 +5,6 @@ from datetime import datetime
 import plotly.express as px
 import database as db
 
-# Initialize the task list in session state
-if 'Tasks' not in st.session_state:
-    st.session_state.Tasks = []
-
 
 if "username" not in st.session_state:
     st.session_state.username = ""
@@ -22,34 +18,39 @@ PROJECT_LIST = db.get_project_name()
 def add_task():
     
     st.write("Please enter the task details below:")
+    with st.form("Add Task"):
     
-    username = st.session_state.username
-    project_name = st.selectbox("Project Name",PROJECT_LIST)
-    task_name = st.text_input("Task name:",placeholder="Please enter your task name here...")
-    start_date = st.date_input("Start date", dt.date.today())
-    end_date = st.date_input("End date", dt.date.today()+dt.timedelta(days=7))
-    task_complete = False
+        username = st.session_state.username
+        project_name = st.selectbox("Project Name",PROJECT_LIST)
+        task_name = st.text_input("Task name:",placeholder="Please enter your task name here...")
+        start_date = st.date_input("Start date", dt.date.today())
+        end_date = st.date_input("End date", dt.date.today()+dt.timedelta(days=7))
+        task_complete = False
 
-    if start_date > end_date:
-        st.warning("Please enter a valid date range!")
-        return
-    
-    if task_name == "":
-        st.warning("Please enter a task name!")
-        return
+        submitted = st.form_submit_button("Add Task")
 
-    # Convert the date objects to strings
-    start_date_str = start_date.strftime("%Y-%m-%d")
-    end_date_str = end_date.strftime("%Y-%m-%d")
+        if start_date > end_date:
+            st.warning("Please enter a valid date range!")
+            return
+
+        if task_name == "":
+            st.warning("Please enter a task name!")
+            return
+
+        # Convert the date objects to strings
+        start_date_str = start_date.strftime("%Y-%m-%d")
+        end_date_str = end_date.strftime("%Y-%m-%d")
+            
+        item = dict(Project= project_name,Task=task_name, Start=start_date_str, Finish=end_date_str, Completed=task_complete)
+
         
-    item = dict(Project= project_name,Task=task_name, Start=start_date_str, Finish=end_date_str, Completed=task_complete)
-    
-    if st.button("Add Task"):
-        st.session_state.Tasks.append(item)
 
-        update_database(username, item)
+        if submitted:
+            #st.session_state.Tasks.append(item)
 
-        st.success("Task was added successfully!")
+            update_database(username, item)
+
+            st.success("Task was added successfully!")
 
 def update_database(username, item):
     existing_work_content = db.get_user_info(username)[0]['work content']
